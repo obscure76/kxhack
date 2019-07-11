f96c3e156c193ee5f87e4d05be5adf3993f3efb5
obscure76 <midhun.nitw@gmail.com>
2019-07-11 13:50:23 -0700

    updated post and comments objects

--- data.py
+++ data.py
@@ -10,3 +10,20 @@ STOP = _("Okay, see you next time!")
 GENERIC_REPROMPT = _("What can I help you with?")
 
 ## TODO: create Post class here
+
+
+class Comment(object):
+    def __init__(self, text, up_votes, down_votes):
+        self.text = text
+        self.up_votes = up_votes
+        self.down_votes = down_votes
+
+
+class Post(object):
+    def __init__(self, id, title='', up_votes=0, down_votes=0, url='', comments=list):
+        self.id = id
+        self.title = title
+        self.up_votes = up_votes
+        self.down_votes = down_votes
+        self.url = url
+        self.comments = comments
