eb20aac212ff7a98f2cbc00de330015f62062eb0
Narendra <panwarn@amazon.com>
2019-07-11 13:29:30 -0700

    code for reddit api

---
+++ reddit_api.py
@@ -0,0 +1,23 @@
+import praw
+reddit = praw.Reddit(client_id='Wffd8ItbvTdUwQ',
+                     client_secret="KJ7pQHa03HGlr1_Mhi35mXeixMw",
+                     user_agent='TestUser')
+print(reddit.read_only)
+
+
+def gettheTitles(keyword, number):
+        titles = []
+        print("\nhere are the " + keyword + " feed" )
+        for submission in reddit.subreddit(keyword).hot(limit=number):
+            titles.append(submission.title)
+        return titles
+
+
+populartitles = gettheTitles("popular",3)
+
+print(populartitles)
+
+hometitles = gettheTitles("home",3)
+
+print (hometitles)
+
